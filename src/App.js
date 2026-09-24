3dc3e3f7a69b6259d12d836ae2013c4e52e966e3
Mohammad Hazem Ali Herzalla <mohammadherzalla591@gmail.com>
2026-09-24 21:50:34 +0300

    todo-list version 1.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+
+import { useRef, useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      const [todos,setTodos]=useState([]);
+      const inputRef=useRef();
+      const handeleAddToDo=()=>{
+      const text=inputRef.current.value;
+      const newItem={completed : false ,text}
+    setTodos([...todos,newItem])
+    console.log(text);
+    inputRef.current.value="";
+  };
+    const handelItemDone=(index)=>{   
+    const newTodos=[...todos];
+    newTodos[index].completed=!newTodos[index].completed;
+    setTodos(newTodos);
+  }
+  const handelDeleteItem=(index)=>{
+    const newTodos=[...todos];
+   newTodos.splice(index,1); 
+   setTodos(newTodos);
+  }
+     return(
+     <div className='App'>
+        <h2>TO  DO  LIST</h2>
+        <ul>
+          {todos.map(({text,completed},index)=>{
+           
+           return(
+           <div className='list-item'>
+           <li className={completed? "done" : ""} 
+           key={index} 
+           onClick={()=> handelItemDone(index) }>{text}</li> 
+           
+           <span onClick={()=>handelDeleteItem(index)}>X</span>
+           </div>
+           );
+         
+          })}
+        </ul>
+        <div className='controls'>
+        <input className='inputText' ref={inputRef} placeholder='Enter What u wana' />
+        <button className='addBtn' onClick={handeleAddToDo}>ADD </button>
+        </div>
+     </div>
+     )
+    
 }
 
 export default App;
